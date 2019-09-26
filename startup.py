b0201c600b62621937fe42676b9d88a9f14692b4
SSILHR\zrehman <zrehman@ssidecisions.com>
2019-09-26 16:07:30 +0500

    create_db command added, as well as docker compose file added.

--- startup.py
+++ startup.py
@@ -3,6 +3,7 @@ from app.main import create_app, db
 from app import blueprint
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from sqlalchemy_utils import create_database, database_exists
 
 app = create_app()
 
@@ -12,6 +13,12 @@ app.register_blueprint(blueprint)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def create_db():
+    url = app.config['SQLALCHEMY_DATABASE_URI']
+    if not database_exists(url):
+        create_database(url)
 
 @manager.command
 def run():
